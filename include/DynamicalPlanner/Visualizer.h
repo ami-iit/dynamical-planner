b3b1e8e40b74f26b94591c0fa33ff6f207049381
Stefano <stefano.dafarra@gmail.com>
2018-10-23 12:58:04 +0200

    Added visualizer.

---
+++ include/DynamicalPlanner/Visualizer.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia
+ * Authors: Stefano Dafarra
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+#ifndef DPLANNER_VISUALIZER_H
+#define DPLANNER_VISUALIZER_H
+
+#include <DynamicalPlanner/State.h>
+#include <iDynTree/Model/Model.h>
+#include <iDynTree/Core/Direction.h>
+#include <iDynTree/Core/Position.h>
+#include <memory>
+
+namespace DynamicalPlanner {
+    class Visualizer;
+}
+
+class DynamicalPlanner::Visualizer {
+public:
+
+    Visualizer();
+
+    ~Visualizer();
+
+    bool setModel(const iDynTree::Model& model);
+
+    bool visualizeState(const DynamicalPlanner::State& stateToVisualize);
+
+    bool visualizeStates(const std::vector<State>& states);
+
+    bool visualizeStates(const std::vector<State>& states, const std::vector<iDynTree::Position>& cameraPosition, const std::vector<iDynTree::Position>& cameraTarget);
+
+    bool setCameraPosition(const iDynTree::Position& cameraPosition);
+
+    bool setCameraTarget(const iDynTree::Position& cameraTarget);
+
+    bool setLightDirection(const iDynTree::Direction& lightDirection);
+
+private:
+
+    class VisualizerImplementation;
+    std::unique_ptr<VisualizerImplementation> m_pimpl;
+};
+
+#endif // DPLANNER_VISUALIZER_H
---
+++ src/Visualizer.cpp
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia
+ * Authors: Stefano Dafarra
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#include <DynamicalPlanner/Visualizer.h>
+#include <iDynTree/Visualizer.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cmath>
+
+
+using namespace DynamicalPlanner;
+
+class Visualizer::VisualizerImplementation {
+public:
+
+    iDynTree::Visualizer viz;
+    iDynTree::Position defaultCameraPosition, defaultCameraTarget;
+
+    VisualizerImplementation() {}
+    ~VisualizerImplementation(){}
+};
+
+Visualizer::Visualizer()
+    : m_pimpl(new VisualizerImplementation)
+{
+    m_pimpl->viz.init();
+    setCameraPosition(iDynTree::Position(1.0, 0.0, 0.5));
+    setCameraTarget(iDynTree::Position(0.4, 0.0, 0.5));
+    double sqrt2 = std::sqrt(2.0);
+    setLightDirection(iDynTree::Direction(-0.5/sqrt2, 0, -0.5/sqrt2));
+}
+
+Visualizer::~Visualizer()
+{
+    m_pimpl->viz.close();
+}
+
+bool Visualizer::setModel(const iDynTree::Model &model)
+{
+    if (m_pimpl->viz.getNrOfVisualizedModels()) {
+        std::cerr << "[ERROR][Visualizer::setModel] Model already set." << std::endl;
+        return false;
+    }
+    bool modelLoaded = m_pimpl->viz.addModel(model, "DynamicalPlannerVisualizer");
+
+    if (!modelLoaded) {
+        std::cerr << "[ERROR][Visualizer::setModel] Failed to set model for visualization." << std::endl;
+        return false;
+    }
+
+    return modelLoaded;
+}
+
+bool Visualizer::visualizeState(const State &stateToVisualize)
+{
+    if (!(m_pimpl->viz.getNrOfVisualizedModels())) {
+        std::cerr << "[ERROR][Visualizer::visualizeState] First you have to load a model." << std::endl;
+        return false;
+    }
+
+    m_pimpl->viz.modelViz(0).setPositions(stateToVisualize.worldToBaseTransform, stateToVisualize.jointsConfiguration);
+    m_pimpl->viz.draw();
+
+    return true;
+}
+
+bool Visualizer::visualizeStates(const std::vector<State> &states)
+{
+    if (!(m_pimpl->viz.getNrOfVisualizedModels())) {
+        std::cerr << "[ERROR][Visualizer::visualizeState] First you have to load a model." << std::endl;
+        return false;
+    }
+
+    for (size_t i = 0; i < states.size(); ++i) {
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        visualizeState(states[i]);
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        if ((i + 1) < states.size()) {
+            std::chrono::milliseconds durationMs(static_cast<int>(std::round((states[i + 1].time - states[i].time)*1000)));
+            std::chrono::milliseconds elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+            if (elapsed < durationMs) {
+                std::this_thread::sleep_for (durationMs - elapsed);
+            }
+        }
+    }
+
+    return true;
+}
+
+bool Visualizer::visualizeStates(const std::vector<State> &states, const std::vector<iDynTree::Position> &cameraPosition, const std::vector<iDynTree::Position> &cameraTarget)
+{
+    if (!(m_pimpl->viz.getNrOfVisualizedModels())) {
+        std::cerr << "[ERROR][Visualizer::visualizeState] First you have to load a model." << std::endl;
+        return false;
+    }
+
+    if (cameraPosition.size() != states.size()) {
+        std::cerr << "[ERROR][Visualizer::visualizeState] The cameraPosition vector should have the same dimension of the states vector." << std::endl;
+        return false;
+    }
+
+    if (cameraTarget.size() != states.size()) {
+        std::cerr << "[ERROR][Visualizer::visualizeState] The cameraTarget vector should have the same dimension of the states vector." << std::endl;
+        return false;
+    }
+
+    for (size_t i = 0; i < states.size(); ++i) {
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        m_pimpl->viz.camera().setPosition(cameraPosition[i]);
+        m_pimpl->viz.camera().setTarget(cameraTarget[i]);
+        visualizeState(states[i]);
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        if ((i + 1) < states.size()) {
+            std::chrono::milliseconds durationMs(static_cast<int>(std::round((states[i + 1].time - states[i].time)*1000)));
+            std::chrono::milliseconds elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+            if (elapsed < durationMs) {
+                std::this_thread::sleep_for (durationMs - elapsed);
+            }
+        }
+    }
+
+    m_pimpl->viz.camera().setPosition(m_pimpl->defaultCameraPosition);
+
+    m_pimpl->viz.camera().setTarget(m_pimpl->defaultCameraTarget);
+
+    return true;
+}
+
+bool Visualizer::setCameraPosition(const iDynTree::Position &cameraPosition)
+{
+    m_pimpl->viz.camera().setPosition(cameraPosition);
+    m_pimpl->defaultCameraPosition = cameraPosition;
+    return true;
+}
+
+bool Visualizer::setCameraTarget(const iDynTree::Position &cameraTarget)
+{
+    m_pimpl->viz.camera().setTarget(cameraTarget);
+    m_pimpl->defaultCameraTarget = cameraTarget;
+    return true;
+}
+
+bool Visualizer::setLightDirection(const iDynTree::Direction &lightDirection)
+{
+    m_pimpl->viz.enviroment().lightViz("sun").setDirection(lightDirection);
+
+    return true;
+
+}
